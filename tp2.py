9d25d4c4a2cc64072af82f0808c02cdf2d2adedf
adzabastella <adzabaayeneveroniquestella@gmail.com>
2025-02-03 09:17:06 +0100

    tp2.py

---
+++ tp2.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import seaborn as sns
+import matplotlib.pyplot as plt  # Importation de matplotlib.pyplot
+
+# Configuration de la page
+st.set_page_config(
+    page_title="Classification des Données Bancaires",
+    page_icon="🏦",  
+    layout="wide", 
+    initial_sidebar_state="expanded"
+)
+
+alt.themes.enable("dark")
+
+# -------------------------
+# Barre latérale
+
+if 'page_selection' not in st.session_state:
+    st.session_state.page_selection = 'a_propos'  # Page par défaut
+
+# Fonction pour mettre à jour page_selection
+def set_page_selection(page):
+    st.session_state.page_selection = page
+
+with st.sidebar:
+    st.title(' 🏦 Classification des Données Bancaires')
+
+    # Navigation par boutons
+    st.subheader("Sections")
+    if st.button("À Propos", use_container_width=True, on_click=set_page_selection, args=('a_propos',)):
+        pass
+    if st.button("Jeu de Données", use_container_width=True, on_click=set_page_selection, args=('jeu_de_donnees',)):
+        pass
+    if st.button("Analyse Exploratoire", use_container_width=True, on_click=set_page_selection, args=('analyse_exploratoire',)):
+        pass
+    if st.button("Nettoyage / Prétraitement des Données", use_container_width=True, on_click=set_page_selection, args=('nettoyage_donnees',)):
+        pass
+    if st.button("Apprentissage Automatique", use_container_width=True, on_click=set_page_selection, args=('apprentissage_automatique',)):
+        pass
+    if st.button("Prédiction", use_container_width=True, on_click=set_page_selection, args=('prediction',)):
+        pass
+    if st.button("Conclusion", use_container_width=True, on_click=set_page_selection, args=('conclusion',)):
+        pass
+
+    # Détails du projet
+    st.subheader("Résumé")
+    st.markdown("""
+        Un tableau de bord interactif pour explorer et classifier les données d'une campagne marketing bancaire.
+
+        -  [Jeu de Données](https://archive.ics.uci.edu/ml/datasets/Bank+Marketing)
+        -  [Notebook Google Colab](https://colab.research.google.com/drive/1KJDBrx3akSPUW42Kbeepj64ZisHFD-NV?usp=sharing)
+        -  [Dépôt GitHub](https://github.com/teguegni/bank-additionnal-full/Streamlit-Bank-Classification-Dashboard)
+
+        *Auteur :* [Kenfack Teguegni Junior](https://jcdiamante.com)
+    """)
+
+# -------------------------
+
+# Charger les données
+try:
+    df = pd.read_csv('bank-additional-full.csv', delimiter=';')
+except FileNotFoundError:
+    st.error("Le fichier 'bank-additional-full.csv' est introuvable. Veuillez vérifier son emplacement.")
+    st.stop()
+
+            st.error(f"Une erreur est survenue : {e}")
